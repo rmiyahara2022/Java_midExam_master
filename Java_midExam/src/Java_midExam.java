3de82214cd4b20cc57faebcf7db1654480dec5be
rmiya <r.miyahara2022@gmail.com>
2023-09-13 21:00:22 +0900

    second commit

--- Java_midExam/src/Java_midExam.java
+++ Java_midExam/src/Java_midExam.java
@@ -2,12 +2,13 @@
 public class Java_midExam {
 
 	public static void main(String[] args) {
-	    // 問1
+	    // 問1 (正解）
 	    String data = "中満悠人"; 
 	    System.out.println(data);
 	    System.out.println("");
 	
-	    // 問2
+	    // 問2（不正解）
+	    // int型であるため、0.1の足し算結果がsumに蓄積されず0となる
 	    int sum = 0;
 	    for (int i = 1; i <= 10; i++) {
             sum += 0.1; 
@@ -16,7 +17,9 @@ public class Java_midExam {
 	    System.out.println(sum);
 	    System.out.println("");
 
-	    // 問3
+	    // 問3（正解）
+	    // num++の場合はnumの表示後に足し算が実行されるのでarrに代入時の値がそのまま表示される
+	    // ++numの場合はnumの表示前に足し算が実行されるのでarrに代入した値にそれぞれ1加算されて表示される
 	    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	    for (int num : arr) { // line 1
 	    	if (num % 2 == 0) {
@@ -27,7 +30,8 @@ public class Java_midExam {
 	    System.out.println("");
 	    System.out.println("");
 	    
-	    // 問4
+	    // 問4（正解）
+	    // 変数quarterのスペルミス
 	    int month = 9;
 	    String japaneseMonth = "";
 	    String quarter = "";
